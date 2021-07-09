75dd22014b61b36f8e9de8736d97cd24d33dacfd
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2021-07-09 19:35:04 +0545

    map bug fixedgit add .

--- lib/application/auth/signup_form/signup_form_bloc.dart
+++ lib/application/auth/signup_form/signup_form_bloc.dart
@@ -20,6 +20,54 @@ class SignupFormBloc extends Bloc<SignupFormEvent, SignupFormState> {
   Stream<SignupFormState> mapEventToState(
     SignupFormEvent event,
   ) async* {
-    //yield* event.
+    yield* event.map(
+      emailChanged: (e) async* {
+        yield state.copyWith(
+          emailAddress: EmailAddress(e.emailStr),
+          authFailOrSuccessOption: none(),
+        );
+      },
+      passwordChanged: (e) async* {
+        yield state.copyWith(
+          password: Password(e.passwordStr),
+          authFailOrSuccessOption: none(),
+        );
+      },
+      signUpWithEmailAndPasswordPressed: (e) async* {
+        yield* _performActionOnAuthFacadeWithEmailAndPassword(
+          _authFacade.signInWithEmailAndPassword,
+        );
+      },
+    );
+  }
+
+  Stream<SignupFormState> _performActionOnAuthFacadeWithEmailAndPassword(
+    Future<Either<AuthFailure, Unit>> Function({
+      required EmailAddress emailAddress,
+      required Password password,
+    })
+        forwardedCall,
+  ) async* {
+    Either<AuthFailure, Unit>? failureOrSuccess;
+
+    final isEmailValid = state.emailAddress.isValid();
+    final isPasswordValid = state.password.isValid();
+
+    if (isEmailValid && isPasswordValid) {
+      yield state.copyWith(
+        isSubmitting: true,
+        authFailOrSuccessOption: none(),
+      );
+
+      failureOrSuccess = await forwardedCall(
+        emailAddress: state.emailAddress,
+        password: state.password,
+      );
+    }
+    yield state.copyWith(
+      isSubmitting: false,
+      showErrorMessagec: true,
+      authFailOrSuccessOption: optionOf(failureOrSuccess),
+    );
   }
 }
